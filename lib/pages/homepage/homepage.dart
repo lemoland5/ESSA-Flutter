bb943e81b90b7e877471479ffbca1ee2adbc0437
Bartosz Bartocha <129062561+BARTEK120B3@users.noreply.github.com>
2024-11-11 12:52:50 +0100

    homepage update and assets update

--- lib/pages/homepage/homepage.dart
+++ lib/pages/homepage/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:essa/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -23,6 +24,9 @@ class _HomepageState extends State<Homepage> {
                 child: Container(
                   color: Colors.amber,
                   child: Text(
+                    style: const TextStyle(
+                      fontSize: 34,
+                    ),
                     'Witaj $_imie',
                   ),
                 ),
@@ -33,9 +37,75 @@ class _HomepageState extends State<Homepage> {
             children: [
               Expanded(
                 child: Container(
-                  color: Colors.red,
-                  child: Text(
-                    'Witaj $_imie',
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.only(left: 20, top: 15, bottom: 15),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/img/color-lightning.svg',
+                            width: 28,
+                            height: 40,
+                          ),
+                          Container(
+                            width: 163,
+                            height: 24,
+                            margin: const EdgeInsets.only(left: 20),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffFFE448),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: const Text('4 kWh'),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/img/color-droplet.svg',
+                            width: 30,
+                            height: 37,
+                          ),
+                          Container(
+                            width: 163,
+                            height: 24,
+                            margin: const EdgeInsets.only(left: 18),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffFFE448),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: const Text('4 kWh'),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/img/gaz.png',
+                            width: 31,
+                            height: 36,
+                          ),
+                          Container(
+                            width: 163,
+                            height: 24,
+                            margin: const EdgeInsets.only(left: 17),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffFFE448),
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: const Text('4 kWh'),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
